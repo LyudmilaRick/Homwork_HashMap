be50f0192c270267913241b0861017e067fc6de6
RickLyudmila <Lrick@yandex.ru>
2021-12-14 23:11:44 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course27/homework_map/HomeworkMapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course27.homework_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course27/homework_map/HomeworkMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course27.homework_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
